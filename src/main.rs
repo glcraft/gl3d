20e73915512956344dc71b78cd2ee0b5b1f2d6e6
Gly <gabin.lefranc@gmail.com>
2023-10-14 18:07:32 +0200

    add environnement and add window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod vulkan;
+mod vkenv;
+mod render;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+
+pub use vulkan::*;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new(); // ignore this for now
+                                       //
+    let vkenv = vkenv::VulkanEnvironment::new(&event_loop).expect("failed to create environment");
+
+    event_loop.run(|event, _, control_flow| match event {
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => {
+            *control_flow = ControlFlow::Exit;
+        }
+        _ => (),
+    });
 }
+
---
+++ src/vkenv.rs
@@ -0,0 +1,155 @@
+use std::sync::Arc;
+use vulkano as vk;
+
+type Result<T> = std::result::Result<T, anyhow::Error>;
+
+#[derive(Debug)]
+pub struct VulkanEnvironment {
+    pub instance: Arc<vk::instance::Instance>,
+    pub queues: Vec<Arc<vk::device::Queue>>,
+    pub swapchain: Arc<vk::swapchain::Swapchain>,
+    pub images: Vec<Arc<vk::image::swapchain::SwapchainImage>>,
+    pub memory_allocator: vk::memory::allocator::GenericMemoryAllocator<Arc<vk::memory::allocator::FreeListAllocator>>,
+    pub window: Arc<winit::window::Window>,
+}
+
+impl VulkanEnvironment {
+    pub fn new(event_loop: &winit::event_loop::EventLoop<()>) -> Result<Self> {
+        let library = vk::VulkanLibrary::new()?;
+        let required_extensions = vulkano_win::required_extensions(&library);
+        let instance = vk::instance::Instance::new(
+            library,
+            vk::instance::InstanceCreateInfo {
+                enabled_extensions: required_extensions,
+                ..Default::default()
+            },
+        )?;
+        let (window, surface) = Self::new_window(event_loop, &instance)?;
+
+        let device_extensions = vk::device::DeviceExtensions {
+            khr_swapchain: true,
+            ..Default::default()
+        };
+
+        let (physical_device, queue_family_index) = Self::new_physical_device(
+            &instance, 
+            &surface,
+            &device_extensions
+        )?;
+        let (logical_device, queues) = Self::new_logical_device(
+            physical_device.clone(), 
+            queue_family_index,
+            device_extensions,
+        )?;
+        let memory_allocator = vk::memory::allocator::StandardMemoryAllocator::new_default(logical_device.clone());
+        let (swapchain, images) = Self::new_swapchain(
+            &window,
+            &physical_device,
+            &logical_device,
+            &surface,
+        )?;
+
+        Ok(VulkanEnvironment {
+            instance,
+            queues,
+            memory_allocator,
+            swapchain,
+            images,
+            window,
+        })
+    }
+    fn new_window(event_loop: &winit::event_loop::EventLoop<()>, instance: &Arc<vk::instance::Instance>) -> Result<(Arc<winit::window::Window>, Arc<vk::swapchain::Surface>)> {
+        use vulkano_win::VkSurfaceBuild;
+        let surface = winit::window::WindowBuilder::new()
+            .build_vk_surface(event_loop, instance.clone())
+            .unwrap();
+        let window = surface
+            .object()
+            .ok_or_else(|| anyhow::anyhow!("failed to get window"))?
+            .clone()
+            .downcast::<winit::window::Window>()
+            .map_err(|_| anyhow::anyhow!("failed to downcast window"))?;
+        Ok((window, surface))
+    }
+    fn new_physical_device(
+        instance: &Arc<vk::instance::Instance>,
+        surface: &vk::swapchain::Surface,
+        required_extensions: &vk::device::DeviceExtensions,
+    ) -> Result<(Arc<vk::device::physical::PhysicalDevice>, u32)> {
+        use vk::device::physical::PhysicalDeviceType;
+        instance
+            .enumerate_physical_devices()?
+            .filter(|p| p.supported_extensions().contains(required_extensions))
+            .filter_map(|p| {
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    // Find the first first queue family that is suitable.
+                    // If none is found, `None` is returned to `filter_map`,
+                    // which disqualifies this physical device.
+                    .position(|(i, q)| {
+                        q.queue_flags.contains(vk::device::QueueFlags::GRAPHICS)
+                            && p.surface_support(i as u32, surface).unwrap_or(false)
+                    })
+                    .map(|q| (p, q as u32))
+            })
+            .min_by_key(|(p, _)| match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                _ => 4,
+            })
+            .ok_or_else(|| anyhow::anyhow!("no suitable physical device found"))
+    }
+    fn new_logical_device(
+        physical_device: Arc<vk::device::physical::PhysicalDevice>,
+        queue_family_index: u32,
+        device_extensions: vk::device::DeviceExtensions,
+    ) -> Result<(Arc<vk::device::Device>, Vec<Arc<vk::device::Queue>>)> {
+        let (logical_device, queues) = vk::device::Device::new(
+            physical_device,
+            vk::device::DeviceCreateInfo {
+                enabled_extensions: device_extensions,
+                queue_create_infos: vec![vk::device::QueueCreateInfo {
+                    queue_family_index,
+                    ..Default::default()
+                }],
+                ..Default::default()
+            },
+        )?;
+        Ok((logical_device, queues.collect()))
+    }
+    fn new_swapchain(
+        window: &Arc<winit::window::Window>,
+        physical_device: &Arc<vk::device::physical::PhysicalDevice>,
+        logical_device: &Arc<vk::device::Device>,
+        surface: &Arc<vk::swapchain::Surface>,
+    ) -> Result<(Arc<vk::swapchain::Swapchain>, Vec<Arc<vk::image::swapchain::SwapchainImage>>)> {
+        let caps = physical_device
+            .surface_capabilities(surface, Default::default())
+            .map_err(|e| anyhow::anyhow!("failed to get surface capabilities: {}", e))?;
+        let dimensions = window.inner_size();
+        let composite_alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
+        let image_format = Some(
+            physical_device
+                .surface_formats(surface, Default::default())
+                .map_err(|e| anyhow::anyhow!("failed to get surface formats: {}", e))?[0]
+                .0,
+        );
+        vk::swapchain::Swapchain::new(
+            logical_device.clone(),
+            surface.clone(),
+            vk::swapchain::SwapchainCreateInfo {
+                min_image_count: caps.min_image_count + 1, // How many buffers to use in the swapchain
+                image_format,
+                image_extent: dimensions.into(),
+                image_usage: vk::image::ImageUsage::COLOR_ATTACHMENT, // What the images are going to be used for
+                composite_alpha,
+                ..Default::default()
+            },
+        )
+        .map_err(|e| anyhow::anyhow!("failed to create swapchain: {}", e))
+    }
+}
+
