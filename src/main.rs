669a5b0a5e58e9fdb9f2372aca632556d984b509
Gly <gabin.lefranc@gmail.com>
2024-10-05 15:49:58 +0200

    Init windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod app;
+use winit::event_loop::{
+        ControlFlow, 
+        EventLoop
+    };
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let event_loop = EventLoop::new().unwrap();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    
+    let mut app = app::App::default();
+    event_loop.run_app(&mut app).map_err(|e| e.into())
 }
