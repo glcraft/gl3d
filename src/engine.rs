8114ab6aa2bdee2ba32aab75d3a34e9ae39db229
Gly <gabin.lefranc@gmail.com>
2024-10-23 03:09:34 +0200

    get graphics and present queues

--- src/engine.rs
+++ src/engine.rs
@@ -1,24 +1,24 @@
-mod surface;
-mod instances;
 pub mod builder;
+mod instances;
+mod surface;
 
+use crate::{error::ApplicationError, utils};
+use ash::{vk, Entry};
 pub use builder::Builder as EngineBuilder;
 use std::{collections::HashSet, ffi::CStr};
-use ash::{vk, Entry};
-use crate::{error::ApplicationError, utils};
 
 pub struct Engine {
     pub instances: instances::Instances,
     pub physical_device: vk::PhysicalDevice,
     pub logical_device: ash::Device,
     pub queues: Queues,
-    pub surface: vk::SurfaceKHR,
+    pub surface: Option<vk::SurfaceKHR>,
 }
 
 pub struct Queues {
-    pub graphics: vk::Queue,
-    pub present: vk::Queue,
-    // pub compute: vk::Queue,
+    pub graphics: Option<vk::Queue>,
+    // pub present: vk::Queue,
+    pub compute: Option<vk::Queue>,
 }
 
 #[derive(Default, Clone, Copy, Debug)]
@@ -29,31 +29,52 @@ struct QueueFamilyIndices {
 }
 
 impl Engine {
-    pub fn new(builder: EngineBuilder, window: &winit::window::Window) -> Result<Engine, ApplicationError> {
+    pub fn new(
+        builder: EngineBuilder,
+        window: &winit::window::Window,
+    ) -> Result<Engine, ApplicationError> {
         let entry = Self::init_entry()?;
 
         let app_info = builder.app_info.unwrap_or_default().into();
 
         let instances = instances::Instances::new(entry, app_info, &builder.device_extensions)?;
 
-        let surface = surface::create_surface(&instances, window)?;
+        let surface = if builder.queue_families.present {
+            Some(surface::create_surface(&instances, window)?)
+        } else {
+            None
+        };
 
         let physical_device = Self::get_physical_device(&instances, &builder.device_extensions)?;
-        
-        let queue_indices = Self::get_queue_family_indices(&instances, &physical_device, &surface)?;
-        let logical_device = Self::create_logical_device(&instances.base, physical_device, queue_indices, &builder.device_extensions)?;
+
+        let queue_indices = Self::get_queue_family_indices(
+            &instances,
+            &physical_device,
+            builder.queue_families,
+            surface.as_ref(),
+        )?;
+        let logical_device = Self::create_logical_device(
+            &instances.base,
+            physical_device,
+            queue_indices,
+            &builder.device_extensions,
+        )?;
 
         let queues = Queues {
-            graphics: unsafe { logical_device.get_device_queue(queue_indices.graphics, 0) },
-            present: unsafe { logical_device.get_device_queue(queue_indices.present, 0) },
-            // compute: unsafe { logical_device.get_device_queue(queue_indices.compute, 0) },
+            graphics: queue_indices
+                .graphics
+                .map(|graphics| unsafe { logical_device.get_device_queue(graphics, 0) }),
+            // present: unsafe { logical_device.get_device_queue(queue_indices.present, 0) },
+            compute: queue_indices
+                .compute
+                .map(|compute| unsafe { logical_device.get_device_queue(compute, 0) }),
         };
         Ok(Engine {
             instances,
             physical_device,
             logical_device,
             queues,
-            surface
+            surface,
         })
     }
 
@@ -61,24 +82,40 @@ impl Engine {
         unsafe { Ok(Entry::load_from("/opt/homebrew/lib/libvulkan.1.dylib")?) }
     }
 
-    fn get_physical_device(instances: &instances::Instances, extensions: &[&CStr]) -> Result<vk::PhysicalDevice, ApplicationError> {
+    fn get_physical_device(
+        instances: &instances::Instances,
+        extensions: &[&CStr],
+    ) -> Result<vk::PhysicalDevice, ApplicationError> {
         let physical_devices = unsafe { instances.base.enumerate_physical_devices()? };
         for physical_device in physical_devices {
             if Self::check_device(instances, &physical_device, extensions)? {
                 return Ok(physical_device);
             }
         }
-        Err(ApplicationError::new("No suitable device found".to_string()))
+        Err(ApplicationError::new(
+            "No suitable device found".to_string(),
+        ))
     }
 
-    fn create_logical_device(instance: &ash::Instance, device: vk::PhysicalDevice, queue_indices: QueueFamilyIndices, extensions: &[&CStr]) -> Result<ash::Device, ApplicationError> {
+    fn create_logical_device(
+        instance: &ash::Instance,
+        device: vk::PhysicalDevice,
+        queue_indices: QueueFamilyIndices,
+        extensions: &[&CStr],
+    ) -> Result<ash::Device, ApplicationError> {
         let features = vk::PhysicalDeviceFeatures {
             ..Default::default()
         };
 
         let mut queue_set = HashSet::with_capacity(3);
-        queue_set.insert(queue_indices.graphics);
-        queue_set.insert(queue_indices.present);
+        if let Some(graphics) = queue_indices.graphics {
+            queue_set.insert(graphics);
+        }
+        if let Some(compute) = queue_indices.compute {
+            queue_set.insert(compute);
+        }
+        // queue_set.insert(queue_indices.graphics);
+        // queue_set.insert(queue_indices.present);
         // queue_set.insert(queue_indices.compute);
         let queue_create_infos = queue_set
             .into_iter()
@@ -89,7 +126,8 @@ impl Engine {
                 ..Default::default()
             })
             .collect::<Vec<_>>();
-        let extensions_ptr: Vec<*const i8> = extensions.iter()
+        let extensions_ptr: Vec<*const i8> = extensions
+            .iter()
             .map(|&slice| slice.as_ptr() as *const i8)
             .collect();
         let device_create_info = vk::DeviceCreateInfo {
@@ -104,61 +142,107 @@ impl Engine {
         let device = unsafe { instance.create_device(device, &device_create_info, None)? };
         Ok(device)
     }
-    fn check_device(instances: &instances::Instances, device: &ash::vk::PhysicalDevice, extensions: &[&CStr]) -> Result<bool, ApplicationError> {
+    fn check_device(
+        instances: &instances::Instances,
+        device: &ash::vk::PhysicalDevice,
+        extensions: &[&CStr],
+    ) -> Result<bool, ApplicationError> {
         unsafe {
             let properties = instances.base.get_physical_device_properties(*device);
             let _features = instances.base.get_physical_device_features(*device);
             // let surface_ = instances.surface.get_physical_device_surface_c(*device, 0, *instances.surface).unwrap();
-            
+
             Ok(Self::check_device_extension(instances, device, extensions)?
-                && (properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU || properties.device_type == vk::PhysicalDeviceType::INTEGRATED_GPU))
+                && (properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU
+                    || properties.device_type == vk::PhysicalDeviceType::INTEGRATED_GPU))
         }
     }
-    fn check_device_extension(instances: &instances::Instances, device: &vk::PhysicalDevice, extensions: &[&CStr]) -> Result<bool, ApplicationError> {
-        let properties = unsafe { instances.base.enumerate_device_extension_properties(*device)? };
+    fn check_device_extension(
+        instances: &instances::Instances,
+        device: &vk::PhysicalDevice,
+        extensions: &[&CStr],
+    ) -> Result<bool, ApplicationError> {
+        let properties = unsafe {
+            instances
+                .base
+                .enumerate_device_extension_properties(*device)?
+        };
         for extension in extensions {
-            let extension_i8_slice = unsafe { std::mem::transmute::<&[u8], &[i8]>(extension.to_bytes()) };
-            if !properties.iter().any(|p| utils::compare_slices(&p.extension_name, extension_i8_slice)) {
+            let extension_i8_slice =
+                unsafe { std::mem::transmute::<&[u8], &[i8]>(extension.to_bytes()) };
+            if !properties
+                .iter()
+                .any(|p| utils::compare_slices(&p.extension_name, extension_i8_slice))
+            {
                 return Ok(false);
             }
         }
         Ok(true)
     }
-    fn get_queue_family_indices(instances: &instances::Instances, device: &vk::PhysicalDevice, fn_check_queue_support: Vec<fn(&ash::vk::PhysicalDevice, u32) -> bool>) -> Result<QueueFamilyIndices, ApplicationError> {
-        let queue_family_properties = unsafe { instances.base.get_physical_device_queue_family_properties(*device) };
-        let mut queues = QueueFamilyIndices::default();
-        for (i, queue_family) in queue_family_properties.iter().enumerate().map(|(i, p)| (i as u32, p)) {
-            for fn_check_queue_support in &fn_check_queue_support {
-                if !fn_check_queue_support(device, i) {
-                    // continue;
+    fn get_queue_family_indices(
+        instances: &instances::Instances,
+        device: &vk::PhysicalDevice,
+        expected_queues: builder::QueueFamilies,
+        surface: Option<&vk::SurfaceKHR>,
+    ) -> Result<QueueFamilyIndices, ApplicationError> {
+        assert!(
+            expected_queues.present && surface.is_some(),
+            "Missing surface while checking for present queue family"
+        );
+        let queue_family_properties = unsafe {
+            instances
+                .base
+                .get_physical_device_queue_family_properties(*device)
+        };
+
+        let mut graphics = None;
+        let mut compute = None;
+        for (i, queue_family) in queue_family_properties
+            .iter()
+            .enumerate()
+            .map(|(i, p)| (i as u32, p))
+        {
+            if expected_queues.graphics.is_some()
+                && graphics.is_none()
+                && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+            {
+                if expected_queues.present
+                    && unsafe {
+                        instances.surface.get_physical_device_surface_support(
+                            *device,
+                            i,
+                            *surface.unwrap(),
+                        )?
+                    }
+                {
+                    graphics = Some(i);
                 }
             }
-            // if graphics.is_none() && queue_family.queue_flags.contains(vk::QueueFlags::GRAPHICS) {
-            //     if present.is_none() && unsafe { instances.surface.get_physical_device_surface_support(*device, i, *surface)? } {
-            //         graphics = Some(i);
-            //     }
-            // }
-            
-            // if queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE) {
-            //     compute = Some(i as u32);
-            // }
-        };
-        Ok(queues)
+            if expected_queues.compute.is_some()
+                && compute.is_none()
+                && queue_family.queue_flags.contains(vk::QueueFlags::COMPUTE)
+            {
+                compute = Some(i);
+            }
+            if graphics.is_some() && compute.is_some() {
+                break;
+            }
+        }
+        Ok(QueueFamilyIndices { graphics, compute })
     }
     #[inline]
     fn get_device_extensions() -> Vec<&'static [u8]> {
-        vec![
-            ash::khr::swapchain::NAME.to_bytes(),
-        ]
+        vec![ash::khr::swapchain::NAME.to_bytes()]
     }
-    
 }
 
 impl Drop for Engine {
     fn drop(&mut self) {
         unsafe {
             self.logical_device.destroy_device(None);
-            self.instances.surface.destroy_surface(self.surface, None);
+            if let Some(surface) = self.surface {
+                self.instances.surface.destroy_surface(surface, None);
+            }
         }
     }
 }
