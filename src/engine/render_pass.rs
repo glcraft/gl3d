70503694003c091fcc067294b36addc2e3d0ef9f
Gly <gabin.lefranc@gmail.com>
2026-01-21 16:36:08 +0100

    change render pass design

--- src/engine/render_pass.rs
+++ src/engine/render_pass.rs
@@ -21,60 +21,45 @@ impl Drop for RenderPass {
     }
 }
 
-pub struct RenderPassBuilder<'a, AttachmentContainer, SubpassContainer, DependencyContainer> {
+pub struct RenderPassBuilder<'a> {
     device: &'a Device,
     flags: vk::RenderPassCreateFlags,
-    attachments: AttachmentContainer,
-    subpasses: SubpassContainer,
-    dependencies: DependencyContainer,
+    attachments: Vec<vk::AttachmentDescription>,
+    subpasses: Vec<SubpassBuilder>,
+    dependencies: Vec<vk::SubpassDependency>,
 }
 
-impl<'a, A, S, D> RenderPassBuilder<'a, A, S, D> {
+impl<'a> RenderPassBuilder<'a> {
     pub fn new() -> Self {
         todo!()
     }
     pub fn build(self) -> Result<vk::RenderPass> {
         todo!()
     }
-    pub fn set_attachments(mut self, value: A) -> Self {
+    pub fn set_attachments(mut self, value: Vec<vk::AttachmentDescription>) -> Self {
         self.attachments = value;
         self
     }
-    pub fn set_subpasses(mut self, value: S) -> Self {
-        self.subpasses = value;
-        self
-    }
-    pub fn set_dependencies(mut self, value: D) -> Self {
-        self.dependencies = value;
-        self
-    }
-}
-impl<'a, S, D> RenderPassBuilder<'a, Vec<vk::AttachmentDescription>, S, D> {
     pub fn add_attachment(mut self, value: vk::AttachmentDescription) -> Self {
         self.attachments.push(value);
         self
     }
-}
-impl<'a, A, D, I, C, R, SuD, P>
-    RenderPassBuilder<'a, A, Vec<SubpassBuilder<'a, I, C, R, SuD, P>>, D>
-{
-    pub fn add_subpass(mut self, value: SubpassBuilder<'_, _, _, _, _, _>) -> Self {
+    pub fn set_subpasses(mut self, value: Vec<SubpassBuilder<'a>>) -> Self {
+        self.subpasses = value;
+        self
+    }
+    pub fn add_subpass(mut self, value: SubpassBuilder) -> Self {
         self.subpasses.push(value);
         self
     }
-}
-impl<'a, A, S> RenderPassBuilder<'a, A, S, Vec<vk::SubpassDependency>> {
+    pub fn set_dependencies(mut self, value: Vec<vk::SubpassDependency>) -> Self {
+        self.dependencies = value;
+        self
+    }
     pub fn add_dependency(mut self, value: vk::SubpassDependency) -> Self {
         self.dependencies.push(value);
         self
     }
-}
-impl<'a, A, S, D> RenderPassBuilder<'a, A, S, D>
-where
-    A: AsRef<[vk::AttachmentDescription]>,
-    S: AsRef<[SubpassBuilder<'a>]>,
-    D: AsRef<[vk::SubpassDependency]>,
-{
     pub fn build(self) -> Result<RenderPass, vk::Result> {
         let subpasses = self
             .subpasses
@@ -99,54 +84,57 @@ where
     }
 }
 
-pub struct SubpassBuilder<
-    'a,
-    InputContainer,
-    ColorContainer,
-    ResolveContainer,
-    DepthStencilContainer,
-    PreserveContainer,
-> {
+pub struct SubpassBuilder {
     flags: vk::SubpassDescriptionFlags,
     bind_point: vk::PipelineBindPoint,
-    input_attachments: InputContainer,
-    color_attachments: ColorContainer,
-    resolve_attachments: ResolveContainer,
-    depth_stencil_attachment: DepthStencilContainer,
-    preserve_attachments: PreserveContainer,
-    _marker: PhantomData<&'a ()>,
+    input_attachments: Vec<vk::AttachmentReference>,
+    color_attachments: Vec<vk::AttachmentReference>,
+    resolve_attachments: Vec<vk::AttachmentReference>,
+    depth_stencil_attachment: Vec<vk::AttachmentReference>,
+    preserve_attachments: Vec<u32>,
 }
 
 impl<'a, I, C, R, D, P> SubpassBuilder<'a, I, C, R, D, P> {
-    pub fn set_input_attachments(mut self, value: I) -> Self {
+    pub fn set_input_attachments(mut self, value: Vec<vk::AttachmentReference>) -> Self {
         self.input_attachments = value;
         self
     }
-    pub fn set_color_attachments(mut self, value: C) -> Self {
+    pub fn add_input_attachment(mut self, input: vk::AttachmentReference) -> Self {
+        self.input_attachments.push(input);
+        self
+    }
+    pub fn set_color_attachments(mut self, value: Vec<vk::AttachmentReference>) -> Self {
         self.color_attachments = value;
         self
     }
-    pub fn set_resolve_attachments(mut self, value: R) -> Self {
+    pub fn add_color_attachment(mut self, color: vk::AttachmentReference) -> Self {
+        self.color_attachments.push(color);
+        self
+    }
+    pub fn set_resolve_attachments(mut self, value: Vec<vk::AttachmentReference>) -> Self {
         self.resolve_attachments = value;
         self
     }
-    pub fn set_depth_stencil_attachments(mut self, value: D) -> Self {
+    pub fn add_resolve_attachment(mut self, resolve: vk::AttachmentReference) -> Self {
+        self.resolve_attachments.push(resolve);
+        self
+    }
+    pub fn set_depth_stencil_attachments(mut self, value: Vec<vk::AttachmentReference>) -> Self {
         self.depth_stencil_attachments = value;
         self
     }
-    pub fn set_preserve_attachments(mut self, value: P) -> Self {
+    pub fn add_depth_stencil_attachment(mut self, depth_stencil: vk::AttachmentReference) -> Self {
+        self.depth_stencil_attachments.push(depth_stencil);
+        self
+    }
+    pub fn set_preserve_attachments(mut self, value: Vec<u32>) -> Self {
         self.preserve_attachments = value;
         self
     }
-}
-impl<'a, I, C, R, D, P> SubpassBuilder<'a, I, C, R, D, P>
-where
-    I: AsRef<[vk::AttachmentReference]>,
-    C: AsRef<[vk::AttachmentReference]>,
-    R: AsRef<[vk::AttachmentReference]>,
-    D: AsRef<[vk::AttachmentReference]>,
-    P: AsRef<[u32]>,
-{
+    pub fn add_preserve_attachment(mut self, preserve: u32) -> Self {
+        self.preserve_attachments.push(preserve);
+        self
+    }
     pub fn make_description(&self) -> vk::SubpassDescription {
         vk::SubpassDescription {
             flags: self.flags,
@@ -160,36 +148,5 @@ where
             p_preserve_attachments: self.preserve_attachments.as_ref().as_ptr(),
             ..Default::default()
         }
-    }
-}
-impl<'a, C, R, D, P> SubpassBuilder<'a, Vec<vk::AttachmentReference>, C, R, D, P> {
-    pub fn add_input_attachment(mut self, input: vk::AttachmentReference) -> Self {
-        self.input_attachments.push(input);
-        self
-    }
-}
-impl<'a, I, R, D, P> SubpassBuilder<'a, I, Vec<vk::AttachmentReference>, R, D, P> {
-    pub fn add_color_attachment(mut self, color: vk::AttachmentReference) -> Self {
-        self.color_attachments.push(color);
-        self
-    }
-}
-impl<'a, I, C, D, P> SubpassBuilder<'a, I, C, Vec<vk::AttachmentReference>, D, P> {
-    pub fn add_resolve_attachment(mut self, resolve: vk::AttachmentReference) -> Self {
-        self.resolve_attachments.push(resolve);
-        self
-    }
-}
-impl<'a, I, C, R, P> SubpassBuilder<'a, I, C, R, Vec<vk::AttachmentReference>, P> {
-    pub fn add_depth_stencil_attachment(mut self, depth_stencil: vk::AttachmentReference) -> Self {
-        self.depth_stencil_attachments.push(depth_stencil);
-        self
-    }
-}
-
-impl<'a, I, C, R, D> SubpassBuilder<'a, I, C, R, D, Vec<u32>> {
-    pub fn add_preserve_attachment(mut self, preserve: u32) -> Self {
-        self.preserve_attachments.push(preserve);
-        self
     }
 }
